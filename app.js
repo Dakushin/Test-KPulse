d1e135507f4cb73ce509a23d0bad8c27a5252cf1
jedelbruel <jeremie.delbruel@etu.uca.fr>
2021-09-01 12:21:11 +0200

    Ajoute de l'évenement click bouton

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+///const switcher = document.querySelector('.btn');
+
+function ChangeText()
+{
+    var b1 = document.getElementById('b1');
+    if(b1.onclick="ChangeText();") {
+        document.getElementById('test').innerHTML = "You pressed button 1";
+    }
+}
