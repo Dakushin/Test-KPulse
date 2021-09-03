af6becf49157c3d11881d0f69370f24caa3a1e1e
jedelbruel <jeremie.delbruel@etu.uca.fr>
2021-09-03 13:43:30 +0200

    ajoue de la list d'entreprise trouver + les boutons

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ function AppelAPI()
 
 var callBackGetSuccess = function(data) {
     console.log("données api", data);
-    /*var element = document.getElementById("test");
-    element.innerHTML = data;*/
+    let list = document.getElementById("list");
+    while (list.firstChild) {
+        list.removeChild(list.lastChild);
+      }
+    for (let index = 0; index < data.total_results; index++) {
+        let newdiv = document.createElement('div');
+        let newP = document.createElement('p');
+        let btn = document.createElement('BUTTON');
+        let pbutton = document.createElement('p');
+        pbutton.textContent = "En savoir plus";
+        newP.textContent = data.etablissement[index].nom_raison_sociale;
+        console.log(newP);
+        list.append(newdiv);
+        newdiv.append(newP);
+        newdiv.append(btn);
+        btn.append(pbutton);
+    }
 }
